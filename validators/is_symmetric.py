43feb7137e227fb70bd7e69f7889411e192cd388
Bejochan <okaganteng894@gmail.com>
2025-09-09 14:04:59 +0000

    feat: tambahkan fungsi untuk memeriksa matriks simetris

---
+++ validators/is_symmetric.py
@@ -0,0 +1,18 @@
+from ..matrix import Matrix
+
+def is_symmetric(matrix):
+    """
+    Memeriksa apakah sebuah matriks adalah matriks simetris.
+    """
+    # 1. Periksa apakah matriks adalah matriks persegi
+    if matrix.rows != matrix.cols:
+        return False
+
+    # 2. Periksa apakah elemen (i, j) sama dengan elemen (j, i)
+    for i in range(matrix.rows):
+        for j in range(matrix.cols):
+            if matrix.data[i][j] != matrix.data[j][i]:
+                return False
+
+    # 3. Jika semua elemen sesuai, matriks adalah simetris
+    return True
