644bde12b73627db7cea6ab2065f3acf726e2ae9
Bejochan <okaganteng894@gmail.com>
2025-09-17 02:37:22 +0000

    feat: tambahkan implementasi SparseMatrix

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import time
 from matrix import Matrix
 from operations.adder import add_matrices
 from operations.multiplier import multiply_matrices
 from utilities import print_matrix
 from exporters.csv_exporter import export_to_csv
+
+def create_sparse_data(size):
+ data = [[0] * size for _ in range(size)]
+ data[0][0] = 1
+ data[size-1][size-1] = 1
+ return data
 
 if __name__ == "__main__":
  matriks_a = Matrix([[1, 2], [3, 4]])
@@ -16,3 +23,12 @@ if __name__ == "__main__":
  matrix_c = Matrix([[10, 20], [30, 40]])
  print("\nMenyimpan Matriks C ke file CSV:")
  export_to_csv(matrix_c, "matriks_c.csv")
+ print("\n--- Menguji Masalah Performa ---")
+ sparse_data_1000 = create_sparse_data(1000)
+ mat_a = Matrix(sparse_data_1000)
+ mat_b = Matrix(sparse_data_1000)
+ start_time = time.time()
+ product_mat = multiply_matrices(mat_a, mat_b)
+ end_time = time.time()
+ print(f"Waktu yang dibutuhkan untuk perkalian: {end_time - start_time:.2f}
+detik")
