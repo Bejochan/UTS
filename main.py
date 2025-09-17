ca818baedc274231800b972751ebc906807a7047
Bejochan <okaganteng894@gmail.com>
2025-09-17 02:13:12 +0000

    feat: tambahkan fungsionalitas ekspor ke CSV

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from matrix import Matrix
 from operations.adder import add_matrices
 from operations.multiplier import multiply_matrices
 from utilities import print_matrix
+from exporters.csv_exporter import export_to_csv
 
 if __name__ == "__main__":
  matriks_a = Matrix([[1, 2], [3, 4]])
@@ -12,4 +13,6 @@ if __name__ == "__main__":
  print("\nHasil Perkalian:")
  hasil_perkalian = multiply_matrices(matriks_a, matriks_b)
  print_matrix(hasil_perkalian)
-
+ matrix_c = Matrix([[10, 20], [30, 40]])
+ print("\nMenyimpan Matriks C ke file CSV:")
+ export_to_csv(matrix_c, "matriks_c.csv")
