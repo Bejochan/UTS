606be729a63766d926da944d759953286b8ba832
Bejochan <okaganteng894@gmail.com>
2025-10-15 11:36:51 +0700

    feat: menambahkan host dan port pada app.py

--- app.py
+++ app.py
@@ -316,4 +316,4 @@ if __name__ == '__main__':
     print("  - GET  /export/dataset    → Export dataset ke CSV")
     print("=" * 60)
     
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
